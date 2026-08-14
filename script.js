722156a7e0281abdd99ebdaaa0782a7f4ec5a83b
pthu30737-arch <pthu30737@gmail.com>
2026-08-14 18:21:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// ========================================
-// PROJECT
-// JAVASCRIPT
-// ========================================
-
-
-// Lấy các thành phần trên trang
-
 const enterButton = document.getElementById("enterButton");
 
 const opening = document.getElementById("opening");
@@ -13,43 +5,26 @@ const opening = document.getElementById("opening");
 const mainPage = document.getElementById("mainPage");
 
 
-// ========================================
-// BẤM VÀO HÌNH PHÚC NGUYÊN
-// ========================================
-
 enterButton.addEventListener("click", function () {
 
-    // Làm màn hình mở đầu mờ dần
+    // Màn hình mở đầu mờ dần
 
     opening.style.opacity = "0";
 
-    opening.style.transition = "opacity 0.8s ease";
 
-
-    // Sau 0.8 giây thì ẩn màn hình mở đầu
+    // Sau 0.8 giây chuyển sang trang chính
 
     setTimeout(function () {
 
         opening.style.display = "none";
 
-
-        // Hiện giao diện chính
-
         mainPage.style.display = "flex";
-
-        mainPage.style.opacity = "0";
-
-        mainPage.style.transition = "opacity 0.8s ease";
-
-
-        // tạo hiệu ứng hiện dần
 
         setTimeout(function () {
 
             mainPage.style.opacity = "1";
 
         }, 50);
-
 
     }, 800);
 
